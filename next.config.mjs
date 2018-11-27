f724557b9288e3de03a695dab6b43bc6fcc88702
Thibaut Levesque <levesque.thibaut@gmail.com>
2018-11-27 16:52:10 +0100

    feat: next.config setup WIP

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import withBundleAnalyzer from "@zeit/next-bundle-analyzer";
+
+const nextConfig = {
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "./bundles/client.html"
+    }
+  },
+  webpack(config) {
+    return config;
+  }
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
