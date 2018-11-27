624766b56d18be64f1227f7db3892d1c1f754f5f
Thibaut Levesque <levesque.thibaut@gmail.com>
2018-11-27 15:55:19 +0100

    feat: first commit

---
+++ helpers/serverHelp.mjs
@@ -0,0 +1,5 @@
+export const azert = "azert";
+
+export const qsdfg = { a: 1, b: 2 };
+
+export const wxcvb = console.log("tàtà is back");
---
+++ pages/a.js
@@ -0,0 +1,10 @@
+import toto from "../helpers/serverHelpers.mjs";
+import { qsdfg, wxcvb } from "../helpers/serverHelp.mjs";
+
+export default () => (
+  <div>
+    <div>{toto}</div>
+    <div>{console.log(qsdfg)}</div>
+    <div>{wxcvb}</div>
+  </div>
+);
