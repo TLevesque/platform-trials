67561c481e0fd0f2ea38dde1c0759c43d928c69e
Thibaut Levesque <levesque.thibaut@gmail.com>
2018-11-27 17:23:07 +0100

    feat: bundle analyzer setup into next config file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ccbdfe3a7ea6cad755c";
+/******/ 	var hotCurrentHash = "f992ebfe77c23ecced9d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import withBundleAnalyzer from "@zeit/next-bundle-analyzer";
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
 const nextConfig = {
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
