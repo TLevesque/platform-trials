12ae6e2864e155d4ae9b24ebbea26508c2b2e3a9
Thibaut Levesque <levesque.thibaut@gmail.com>
2018-11-29 04:30:03 +0100

    feat: server modules WIP

--- next.config.js
+++ next.config.js
@@ -11,32 +11,50 @@ const {
 const { ANALYZE } = process.env;
 const withTM = require("next-plugin-transpile-modules");
 
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : require("next-server/constants");
+
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_PRODUCTION_SERVER) {
+//     // Config used to run in production.
+//     return {};
+//   }
+
+//   const withCSS = require("@zeit/next-css");
+
+//   return withCSS();
+// };
+
 const nextConfig = {
-  webpack: config => {
-    config.plugins = config.plugins || [];
-    config.plugins = [
-      ...config.plugins,
-      new Dotenv({
-        path: path.join(__dirname, ".env"),
-        systemvars: true
-      })
-    ];
-    if (ANALYZE) {
-      config.plugins.push(new WebpackBundleSizeAnalyzerPlugin("stats.txt"));
-    }
-    config.externals = Array.isArray(config.externals)
-      ? config.externals.map(fn =>
-          typeof fn === "function"
-            ? (context, request, callback) => {
-                if (request === "lodash-es") {
-                  return callback(null, "commonjs lodash");
+  webpack: (phase, { config }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+      config.plugins = config.plugins || [];
+      config.plugins = [
+        ...config.plugins,
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true
+        })
+      ];
+      if (ANALYZE) {
+        config.plugins.push(new WebpackBundleSizeAnalyzerPlugin("stats.txt"));
+      }
+      config.externals = Array.isArray(config.externals)
+        ? config.externals.map(fn =>
+            typeof fn === "function"
+              ? (context, request, callback) => {
+                  if (request === "lodash-es") {
+                    return callback(null, "commonjs lodash");
+                  }
+                  return fn(context, request, callback);
                 }
-                return fn(context, request, callback);
-              }
-            : fn
-        )
-      : config.externals;
-    return config;
+              : fn
+          )
+        : config.externals;
+      return config;
+    }
   }
 };
 
