facbbfdd64e50b9324c222e5d879e1c6a427ef55
Thibaut Levesque <levesque.thibaut@gmail.com>
2018-12-03 16:24:59 +0100

    feat: working with all plugins

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b37dfb173def9c5beadb";
+/******/ 	var hotCurrentHash = "328800e9846fe6d75b30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -1,74 +1,77 @@
-const withPlugins = require("next-compose-plugins");
-const sass = require("@zeit/next-sass");
-require("dotenv").config();
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
-const images = require("next-images");
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const {
-  WebpackBundleSizeAnalyzerPlugin
-} = require("webpack-bundle-size-analyzer");
-const withTM = require("next-plugin-transpile-modules");
-const { ANALYZE } = process.env;
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : require("next-server/constants");
 
-const nextConfig = {
-  webpack: config => {
-    config.plugins = config.plugins || [];
-    config.plugins = [
-      ...config.plugins,
-      new Dotenv({
-        path: path.join(__dirname, ".env"),
-        systemvars: true
-      })
-    ];
-    if (ANALYZE) {
-      config.plugins.push(new WebpackBundleSizeAnalyzerPlugin("stats.txt"));
-    }
-    config.externals = Array.isArray(config.externals)
-      ? config.externals.map(fn =>
-          typeof fn === "function"
-            ? (context, request, callback) => {
-                if (request === "lodash-es") {
-                  return callback(null, "commonjs lodash");
-                }
-                return fn(context, request, callback);
-              }
-            : fn
-        )
-      : config.externals;
-    return config;
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
   }
-};
 
-module.exports = withPlugins(
-  [
-    [sass],
-    [images],
-    [
-      withBundleAnalyzer,
-      {
-        analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-        analyzeBrowser: ["browser", "both"].includes(
-          process.env.BUNDLE_ANALYZE
-        ),
-        bundleAnalyzerConfig: {
-          server: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/server.html"
+  const sass = require("@zeit/next-sass");
+  const images = require("next-images");
+  require("dotenv").config();
+  const path = require("path");
+  const Dotenv = require("dotenv-webpack");
+  const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+  const {
+    WebpackBundleSizeAnalyzerPlugin
+  } = require("webpack-bundle-size-analyzer");
+  const withTM = require("next-plugin-transpile-modules");
+  const { ANALYZE } = process.env;
+
+  return withBundleAnalyzer(
+    withTM(
+      images(
+        sass({
+          analyzeServer: ["server", "both"].includes(
+            process.env.BUNDLE_ANALYZE
+          ),
+          analyzeBrowser: ["browser", "both"].includes(
+            process.env.BUNDLE_ANALYZE
+          ),
+          bundleAnalyzerConfig: {
+            server: {
+              analyzerMode: "static",
+              reportFilename: "../bundles/server.html"
+            },
+            browser: {
+              analyzerMode: "static",
+              reportFilename: "./bundles/client.html"
+            }
           },
-          browser: {
-            analyzerMode: "static",
-            reportFilename: "./bundles/client.html"
+          transpileModules: ["lodash-es"],
+          webpack: config => {
+            config.plugins = config.plugins || [];
+            config.plugins = [
+              ...config.plugins,
+              new Dotenv({
+                path: path.join(__dirname, ".env"),
+                systemvars: true
+              })
+            ];
+            if (ANALYZE) {
+              config.plugins.push(
+                new WebpackBundleSizeAnalyzerPlugin("stats.txt")
+              );
+            }
+            config.externals = Array.isArray(config.externals)
+              ? config.externals.map(fn =>
+                  typeof fn === "function"
+                    ? (context, request, callback) => {
+                        if (request === "lodash-es") {
+                          return callback(null, "commonjs lodash");
+                        }
+                        return fn(context, request, callback);
+                      }
+                    : fn
+                )
+              : config.externals;
+            return config;
           }
-        }
-      }
-    ],
-    [
-      withTM,
-      {
-        transpileModules: ["lodash-es"]
-      }
-    ]
-  ],
-  nextConfig
-);
+        })
+      )
+    )
+  );
+};
